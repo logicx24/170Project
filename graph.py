afa04900dfdecef8b64cc36f895b4771892dd01c
Sahil Patel <spatel7@berkeley.edu>
2015-04-28 23:42:07 -0700

    Changed the graph file

--- graph.py
+++ graph.py
@@ -1,18 +1,20 @@
 
 class Graph(object):
 
-	def __init__(self, graph_array, n, red_blue_str):
-		self.adj_matrix = graph_array
-		self.num_nodes = n 
-		self.colors = red_blue_str.split("")
+	# the file in this case is passed in as one big string seperated by \n
+	def __init__(self, file):
+		lines = file.split("\n")
+		last_index = len(lines) - 1
+		self.num_nodes = int(lines[0])
+		self.colors = lines[last_index].split("")
+		self.weights_matrix = [[int(weight) for weight in line.split(" ")] for line in lines[1:last_index]]
 
-	def add_edge(self, weight, node1, node2):
-		self.adj_matrix[node1][node2] = weight
-		self.adj_matrix[node2][node1] = weight
+	def set_weight(self, weight, node1, node2):
+		self.weights_matrix[node1][node2] = weight
+		self.weights_matrix[node2][node1] = weight
 
-	def remove_edge(self, node1, node2):
-		self.adj_matrix[node1][node2] = None
-		self.adj_matrix[node2][node1] = None
+	def get_weight(self, node1, node2):
+		return self.weights_matrix[node1][node2]
 
 	def is_valid_sequence(self, seq):
 		color_count = 0
@@ -24,25 +26,13 @@ class Graph(object):
 			if color_count > 3:
 				return False
 			last_color = self.colors[node]
-		return self.is_valid_path(seq)
-
-	def is_valid_path(self, path):
-		for u, v in zip(path[:-1], path[1:]):
-			if not self.has_edge(u, v):
-				return False
 		return True
 
-	def has_edge(self, node1, node2):
-		return self.adj_matrix[node1][node2] != None
-
 	def is_valid_hamiltonian(self, path):
-		return len(list(set(path))) == self.num_nodes and len(list(set(path))) == len(path) and self.is_valid_sequence(path)
+		return len(list(set(path))) == self.num_nodes and len(list(set(path))) == len(path)
 
 
 
-
-
-				
 
 
 
