f5610c83be1d4361428def6e7d36bdb05d03602c
Mihir Patil <mihir.patil@berkeley.edu>
2015-05-01 00:58:32 -0700

    Added some tests

--- tests/test_dynamic.py
+++ tests/test_dynamic.py
@@ -23,13 +23,13 @@ class TestDynamicSolver(unittest.TestCase):
         self._check(actual_path, expected_path)
         self._check(actual_dist, expected_dist)
 
-    def test_ten_nodes(self):
+    def test_ten_nodes_valid_colors(self):
 
-        graph = Graph(open(TEST_PATH + 'ten_nodes.in').read())
+        graph = Graph(open(TEST_PATH + 'ten_nodes1.in').read())
         actual_path, actual_dist = dynamicSolver(graph)
 
-        expected_path = [5, 4, 2, 8, 0, 3, 9, 6, 7, 1]
-        expected_dist = 159.0
+        # expected_path = [5, 4, 2, 8, 0, 3, 9, 6, 7, 1]
+        # expected_dist = 159.0
 
-        self._check(actual_path, expected_path)
-        self._check(actual_dist, expected_dist)
+        self._check(graph.is_valid_coloring(actual_path), True)
+        # self._check(actual_dist, expected_dist)
