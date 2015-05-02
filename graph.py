9df81fd28edb15e692c7efdf6b4fcc72138cef21
Sahil Patel <spatel7@berkeley.edu>
2015-05-02 15:36:11 -0700

    colors stuff

--- graph.py
+++ graph.py
@@ -102,6 +102,23 @@ class Graph(object):
 			return False
 		return edge[0] in path and edge[1] in path
 
+	def is_last_color(self, path, edge):
+		new_path = self.append_edge(path, edge)
+		num_blues = sum([1 for node in new_path if self.is_blue(node)])
+		num_reds = sum([1 for node in new_path if self.is_red(node)])
+		num_blues_left = self.num_nodes / 2 - num_blues
+		num_reds_left = self.num_nodes / 2 - num_reds
+
+		if num_blues_left == 0:
+			return num_reds_left <= 3
+		if num_reds_left == 0:
+			return num_blues_left <= 3
+
+		if num_reds_left > num_blues_left:
+			return (num_reds_left - 3) / num_blues_left <= 3
+
+		return (num_blues_left - 3) / num_reds_left <= 3
+
 	def valid_options(self, path):
 		""" Given a path, the list of edges that extend from the endpoints of the graph
 		and do not create cycles AND do not violate the max-3-node constraint."""
@@ -111,7 +128,7 @@ class Graph(object):
 		#	if last_three_node_colors[0] == last_three_node_colors[1] == last_three_node_colors[2]:
 		#		next_valid_colors.remove(last_three_node_colors[0])
 
-		return [edge for edge in self.all_edges if not self.creates_cycle(edge, path) and (path[0] in edge or path[-1] in edge) and self.is_valid_coloring(self.append_edge(path, edge))] #(self.colors[path[0]] in next_valid_colors or self.colors[path[-1]] in next_valid_colors)]
+		return [edge for edge in self.all_edges if not self.creates_cycle(edge, path) and (path[0] in edge or path[-1] in edge) and self.is_valid_coloring(self.append_edge(path, edge)) and not self.is_last_color(path, edge)] #(self.colors[path[0]] in next_valid_colors or self.colors[path[-1]] in next_valid_colors)]
 
 	# LOOOOOL OMG GET IT?????????????
 	def append_edge(self, path, edge):
@@ -141,6 +158,8 @@ class Graph(object):
 		elif heuristic is Graph.SMART:
 			heuristic_func = self.smart_heuristic
 
+		cheapest_path = None
+
 		# choose the first edge just as the cheapest one in the graph
 		path = list(min(self.all_edges, key=lambda edge: self.get_weight(edge[0], edge[1])))
 
@@ -149,6 +168,8 @@ class Graph(object):
 
 			# using the passed in heuristic, pass in the path up to that point
 			new_edge = heuristic_func(path)
+			if not new_edge:
+				continue
 
 			# add the new edge to the proper side of the path
 			new_node = new_edge[0]
@@ -177,8 +198,13 @@ class Graph(object):
 
 	def general_heuristic(self, path):
 		edges = self.valid_options(path)
+		if len(edges) == 0:
+			return None
 		return min(edges, key=lambda edge: self.get_weight(edge[0], edge[1]))
 
+	# more like slightly educated heuristic. like barely high school graduate who enlisted in the marines
+	# and served 4 years, left with a honorable discharge, became a carpenter in a quiet suburban neighborhood
+	# in the midwest, lived a simple, happy life.
 	def smart_heuristic(self, path):
 		left_endpoint, right_endpoint = path[0], path[-1]
 		weighter = lambda edge: self.get_weight(edge[0], edge[1])
