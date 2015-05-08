2c9f962d2c30969e50f2cf57f3f0eae15363439f
Mihir Patil <mihir.patil@berkeley.edu>
2015-05-08 01:05:58 -0700

    modified answers/answer.out to do dynamic programming for 16 node graphs

--- dynamic_improver.py
+++ dynamic_improver.py
@@ -9,7 +9,7 @@ NUM_FILES = 495
 for file_to_change in range(1, NUM_FILES + 1):
 	graph = Graph(open("instances/{0}.in".format(file_to_change)).read())
 
-	if graph.num_nodes == 14: 
+	if graph.num_nodes == 16: 
 		print "Running Dynamic Solver on {0}.in of size {1}".format(file_to_change, graph.num_nodes)  
 		new_path, new_cost = dynamicSolver(graph)
 		compare_paths(file_to_change, new_path, print_output=True)
