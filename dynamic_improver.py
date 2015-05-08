f3a006982ce906a25eb24e2b093e29a04e44360e
Mihir Patil <mihir.patil@berkeley.edu>
2015-05-08 03:30:14 -0700

    minor change to dynamic solver.

--- dynamic_improver.py
+++ dynamic_improver.py
@@ -9,8 +9,8 @@ NUM_FILES = 495
 for file_to_change in range(1, NUM_FILES + 1):
 	graph = Graph(open("instances/{0}.in".format(file_to_change)).read())
 
-	if graph.num_nodes == 16: 
+	if graph.num_nodes == 20: 
 		print "Running Dynamic Solver on {0}.in of size {1}".format(file_to_change, graph.num_nodes)  
-		new_path, new_cost = quickDynamicSolver(graph)
+		new_path, new_cost = dynamicSolverParallel(graph)
 		compare_paths(file_to_change, new_path, print_output=True)
 		print "\n"
